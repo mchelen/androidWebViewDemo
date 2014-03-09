fd20e73a82d3192a38681b413b4d3cbecdb737e6
Mike Chelen <michael.chelen@gmail.com>
2014-03-09 09:59:08 -0400

    remove generated files

--- gen/R.java.d
+++
@@ -1,4 +0,0 @@
-/home/mikechelen/Documents/Dropbox/Public/androidWebViewDemo/gen/com/example/webviewdemo/R.java \
- : /home/mikechelen/Documents/Dropbox/Public/androidWebViewDemo/res/values/strings.xml \
-/home/mikechelen/Documents/Dropbox/Public/androidWebViewDemo/res/layout/main.xml \
-/home/mikechelen/Documents/Dropbox/Public/androidWebViewDemo/bin/AndroidManifest.xml \
